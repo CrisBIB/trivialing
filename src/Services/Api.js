5942025da5e49cd8e7146fe1abf58b11cd606803
CCrisBIB <cristinabiglesias@gmail.com>
2021-05-09 12:00:19 +0200

    Add .catch(error) to find error in fetch

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,16 +1,25 @@
 const getRandomNumberFromApi = () => {
-  return fetch(`//numbersapi.com/random/trivia?json`)
+  return fetch("//numbersapi.com/random/trivia?json")
     .then((response) => response.json())
     .then((dataApi) => {
+      console.log(dataApi);
       return dataApi;
-    });
+    })
+    .catch((errorRandomNumber) =>
+      console.log(`Ha sucedido un error: ${errorRandomNumber}`)
+    );
 };
+
 const getFragmentNumberFromApi = (number) => {
   return fetch(`//numbersapi.com/${number}/trivia?fragment`)
     .then((response) => response.text())
     .then((dataApi) => {
+      console.log(dataApi);
       return dataApi;
-    });
+    })
+    .catch((errorFragmentNumber) =>
+      console.log(`Ha sucedido un error: ${errorFragmentNumber}`)
+    );
 };
 
 export default {
